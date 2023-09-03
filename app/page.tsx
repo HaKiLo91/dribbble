2d6961ad55089c271dae50bdd77d11c6e58c4e9a
hophucha <hophucha47@gmail.com>
2023-09-03 18:04:00 +0700

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+const Home = () => {
+    return (
+        <section className="flex-start flext-col paddings mb-6">
+            <h2>Categories</h2>
+            <h2>Posts</h2>
+            <h2>LoadMore</h2>
+        </section>
+    );
+};
+
+export default Home;
